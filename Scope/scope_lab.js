a7d56a74837e6ecf4b6c5898339b935d414854ce
Khin <khinmyat.23@ichat.sp.edu.sg>
2025-06-21 10:27:01 -0400

    scope_lab_test

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -17,13 +17,32 @@ console.log(globalConst); // Output: "I'm a global constant"
 //Block Scope
 // console.log(blockVar);
 // console.log(blockLet);
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-    show();
+// function show(){
+//     var functionVar = "I'm a block-scoped var";
+//     let functionLet = "I'm a block-scoped let";
+//     const functionConst = "I'm a block-scoped const";
+//     }
+//     show();
     
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+//     console.log(functionVar); // Throws ReferenceError
+//     console.log(functionLet); // Throws ReferenceError
+//     console.log(functionConst); // Throws ReferenceError
+
+{
+    let blkLet = 1;
+    var blkVar = 2;
+    const blkConst = 3;
+    // blkLet = 1;
+    // blkVar = 2;
+    // blkConst = 3;
+    // console.log(blkLet);
+    // console.log(blkVar);
+    // console.log(blkConst);
+}
+
+    blkLet = 4;
+    blkVar = 5;
+    blkConst = 6;
+    console.log(blkLet);
+    console.log(blkVar);
+    console.log(blkConst);
