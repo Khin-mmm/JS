a92836e29d786171270f5dd6b85b861b82c3f666
Khin <khinmyat.23@ichat.sp.edu.sg>
2025-06-22 10:08:41 -0400

    followersCount:reset button and both closure and hoisting

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,18 +1,33 @@
-let count = 0; // Initialize count to 0
-function increaseCount() {
-  count++; // Increment the count by 1
-  displayCount(); // Display the count
-  checkCountValue(); // Check count value and display messages
-}
+function followersCounter(){
+    let count = 0; // Initialize count to 0
+    function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+    }
+    function checkCountValue() {
+        if (count === 10) {
+          alert("Your Instagram post gained 10 followers! Congratulations!");
+        } else if (count === 20) {
+          alert("Your Instagram post gained 20 followers! Keep it up!");
+        }else if (count === 0){
+          alert("Followers Counter has been reset");
+        }
+      }
+      
+      function displayCount() {
+      document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+      }
+      
+      function resetCount() {
+          count = 0;
+          displayCount();
+          checkCountValue();
+      }
 
-function checkCountValue() {
-  if (count === 10) {
-    alert("Your Instagram post gained 10 followers! Congratulations!");
-  } else if (count === 20) {
-    alert("Your Instagram post gained 20 followers! Keep it up!");
-  }
+    return {
+        increase: increaseCount,
+        reset: resetCount
+    };
 }
-
-function displayCount() {
-document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
-}
+const counter= followersCounter();
